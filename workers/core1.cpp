1e992563ddd24143e8b127da9cdd75f8a6b28886
Kuba <toja@example.com>
2023-12-02 15:33:38 +0100

    odczytywanie wszystkich ramek z pliku

--- workers/core1.cpp
+++ workers/core1.cpp
@@ -22,29 +22,16 @@ Core1::Core1()
 Core1::~Core1() { };
 
 
-
-
-
-
-
-
-
-
-string prepare_string(){
-    ifstream infile("train_data/2022_07_22_08_10_Gliwice_Czestochowa_data");
+string prepare_string(string line){
     string data;
-    getline(infile, data);  // assign one line to "data"
 
-    data = data.substr(data.find("=") + 1);  // delete miliseconds and "DATA="
+    data = line.substr(line.find("=") + 1);  // delete miliseconds and "DATA="
 
     data.erase(remove(data.begin(), data.end(), '|'), data.end());  // delete "|"
     data.erase(remove(data.begin(), data.end(), ';'), data.end());  // delete ";"
 
     return data;
 }
-
-
-
 
 
 array<string, 3> extract_info(int info_index){
@@ -63,9 +50,6 @@ array<string, 3> extract_info(int info_index){
 }
 
 
-
-
-
 void transform_info(string data, string info_name, int info_pos, int info_len){  // do zmiany- co gdy "len" inne niż "8"
     string info = data.substr(info_pos/4, info_len/4);  // find wanted info in string 
     int info_int;
@@ -73,21 +57,23 @@ void transform_info(string data, string info_name, int info_pos, int info_len){ 
     fprintf(stderr, "%s: %d \n", info_name.c_str(), info_int);  // print info
 }
 
-
-
-
-
 void* Core1::run()
 { 
-    for(int i=0; i<6; i++){
-        string data = prepare_string();
+    ifstream readFromFile("train_data/2022_07_22_08_10_Gliwice_Czestochowa_data");
+    string line;
+    while (!readFromFile.eof())
+    {
+        getline(readFromFile,line);
+        for(int i=0; i<6; i++)
+        {
+            string data = prepare_string(line);
 
-        array<string, 3> current_info = extract_info(i);
+            array<string, 3> current_info = extract_info(i);
         
-        transform_info( data, current_info[0], stoi(current_info[1]), stoi(current_info[2]) );
+            transform_info( data, current_info[0], stoi(current_info[1]), stoi(current_info[2]) );
+        }
+        sleep(1);
     }
-
-
     return NULL;
 }
 
