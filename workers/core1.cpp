848e6bdc2b5247b8108e75e51c4cf974ffea404c
Myetekk <99021484+Myetekk@users.noreply.github.com>
2023-12-08 17:17:46 +0100

    wywalenie niepotrzebnych plików, zmiany estetyczne

--- workers/core1.cpp
+++ workers/core1.cpp
@@ -1,215 +1,265 @@
-#include <iostream>
-#include <unistd.h>
-#include <string>
-#include <inttypes.h>
-#include <algorithm>
-#include <fstream>
-#include <iomanip>
+// #include <iostream>
+// #include <unistd.h>
+// #include <string>
+// #include <inttypes.h>
+// #include <algorithm>
+// #include <fstream>
+// #include <iomanip>
 
-#include "../nlohman/json.hpp"
-#include "core1.h"
-#include "../utils/netfunctions.h"
+// #include "../nlohman/json.hpp"
+// #include "core1.h"
+// #include "../utils/netfunctions.h"
 
-using json = nlohmann::json;
+// using json = nlohmann::json;
 
-using namespace std;
+// using namespace std;
 
-#define MATH_PI 3.14159265358979323846
-#define earthRadiusKm 6371.0
-Core1::Core1()
-{
+// #define MATH_PI 3.14159265358979323846
+// #define earthRadiusKm 6371.0
 
-}
+// Core1::Core1()
+// {
 
-Core1::~Core1() { };
+// }
 
-string prepare_string(string line){
-    string data;
-
-    data = line.substr(line.find("=") + 1);  // delete miliseconds and "DATA="
-
-    data.erase(remove(data.begin(), data.end(), '|'), data.end());  // delete "|"
-    data.erase(remove(data.begin(), data.end(), ';'), data.end());  // delete ";"
-
-    return data;
-}
-array<string, 3> extract_info(int info_index){
-    std::ifstream infile("train_data/data_instrukcja.json");
-    json data = json::parse(infile);  // parse json
-    json firstAwiaParams = data["AwiaParams"][0];  // enter table AwiaParams
-    json paramsArray = firstAwiaParams["params"][info_index];  // enter table Params
-    array<string, 3> info;
-    info[0] = paramsArray.value("key", "not found");  // get value "key"
-    info[1] = to_string(paramsArray.at("awiabit"));  // get value "awiabit"
-    info[2] = to_string(paramsArray.at("len"));  // get value "len"
-    return info;
-}
-void byte_swap(string& text){//Funtion swapping bytes from little endian to big endian, works only for numbers of length 8,16,24 and so on....
-        int length = text.length();
-        if(length<=2)return;//no need to change bytes
-        int x = length/2;
-        char temp;
-        for(int i=0;i<x;i+=2){
-                temp=text[i];
-                text[i]=text[length-2-i];
-                text[length-2-i]=temp;
-                temp=text[i+1];
-                text[i+1]=text[length-1-i];
-                text[length-1-i]=temp;
-        }
-}
-void transform_info(string data, string info_name, int info_pos, int info_len)
-{ 
-    // do zmiany- co gdy "len" inne niż "8"
-    // W tej funkcji trzeba poprawić odczytywanie współrzędnych szerokości i wysokości geograficznej
-    info_pos = info_pos/4;
-    info_len = info_len/4;
-    if(info_len<=2){
-        string info = data.substr(info_pos, info_len);  // find wanted info in string 
-        int info_int;
-        
-        istringstream(info) >> hex >> info_int;  // convert info to decimal
-        fprintf(stderr, "%s: %d \n", info_name.c_str(), info_int);  // print info
-    }
-    else{
-        string info = data.substr(info_pos,info_len);
-        byte_swap(info);
-        union ulf
-        {
-            unsigned long ul;
-            float f;
-        };
-        ulf u;
-        string str = info;
-        stringstream ss(str);
-        ss >> hex >> u.ul;
-        float f = u.f;
-        fprintf(stderr, "%s: %f \n", info_name.c_str(), f);
-    }
-}
-void transform_info(string data, string info_name, int info_pos, int info_len,float &coord)
-{ 
-    // do zmiany- co gdy "len" inne niż "8"
-    // W tej funkcji trzeba poprawić odczytywanie współrzędnych szerokości i wysokości geograficznej
-    info_pos = info_pos/4;
-    info_len = info_len/4;
-    if(info_len<=2){
-        string info = data.substr(info_pos, info_len);  // find wanted info in string 
-        int info_int;
-        istringstream(info) >> hex >> info_int;  // convert info to decimal
-        fprintf(stderr, "%s: %d \n", info_name.c_str(), info_int);  // print info
-    }
-    else if(info_pos==52){
-        string info = data.substr(info_pos, info_len);  // find wanted info in string 
-        byte_swap(info);
-        float info_float;
-        int info_int;
-        istringstream(info) >> hex >> info_int;  // convert info to decimal
-        coord = info_int;
-
-    }
-    else{
-        string info = data.substr(info_pos,info_len);
-        byte_swap(info);
-        union ulf
-        {
-            unsigned long ul;
-            float f;
-        };
-        ulf u;
-        string str = info;
-        stringstream ss(str);
-        ss >> hex >> u.ul;
-        float f = u.f;
-        coord = u.f;
-        fprintf(stderr, "%s: %f \n", info_name.c_str(), f);
-    }
-}
-void getCoords(string data,float& x, float& y,int& distance){
-    //reading latitude from file
-    float distanceF;
-    int distanceI;
-    array<string, 3> current_info = extract_info(14);
-    transform_info(data,current_info[0],stoi(current_info[1]),stoi(current_info[2]),x);
-    //reading longitude from file
-    current_info = extract_info(15);
-    transform_info(data, current_info[0], stoi(current_info[1]), stoi(current_info[2]),y);
-    current_info = extract_info(18);
-    //reading distance from file
-    transform_info(data, current_info[0], stoi(current_info[1]), stoi(current_info[2]),distanceF);
-    distance = distanceF;
-}
-// This function converts decimal degrees to radians
-double deg2rad(double deg) {
-  return (deg * MATH_PI / 180);
-}
-
-//  This function converts radians to decimal degrees
-double rad2deg(double rad) {
-  return (rad * 180 / MATH_PI);
-}
-int32_t distanceCal(double lat1, double lon1, double lat2, double lon2, int64_t *odist)
-{
-    if ((lat1 == lat2) && (lon1 == lon2))
-    {
-        *odist = 0;
-        return (int32_t)Result::SUCCESS;
-    }
-    double lat1r, lon1r, lat2r, lon2r, u, v;
-    lat1r = deg2rad(lat1);
-    lon1r = deg2rad(lon1);
-    lat2r = deg2rad(lat2);
-    lon2r = deg2rad(lon2);
-    u = sin((lat2r - lat1r)/2);
-    v = sin((lon2r - lon1r)/2);
-    *odist = 1000 * 1000 * 2.0 * earthRadiusKm * asin(sqrt(u * u + cos(lat1r) * cos(lat2r) * v * v));
-
-    return (int32_t)Result::SUCCESS;
-}
-//Reading data from file
-void readData(vector<string>& route){
-    fstream f1("train_data/route.json");
-    json j = json::parse(f1);
-    route = j.get<vector<string>>();
-
-}
-int findFirstStation(float& x,float& y){
-    return 0;
+// Core1::~Core1() { };
 
 
-}
-void* Core1::run()
-{ 
-    ifstream readFromFile("train_data/2022_07_22_08_10_Gliwice_Czestochowa_data");
-    string line;
-    float x,y;
-    int distance;
-    int firststation = -1;
-    int state = 0;
-    vector<string> route;
-    while (!readFromFile.eof())
-    {
-        getline(readFromFile,line);
-        string data = prepare_string(line);
-        for(int i=0; i<6; i++)
-        {
-            array<string, 3> current_info = extract_info(i);  
-            transform_info( data, current_info[0], stoi(current_info[1]), stoi(current_info[2]));
-        }
-        //reading coords, and distance from file
-        getCoords(data,x,y,distance);
-        cout<<setprecision(8)<<endl<<"X:"<<x<<endl<<"Y:"<<y<<endl;
-        cout<<"DistanceFromStart: "<<distance<<endl;
-        cout << "\n";
-        if(firststation == -1){
-            findFirstStation(x,y);
 
-        }
-        readData(route);
-        sleep(1);
-    }
-    return NULL;
-}
+
+
+// string prepare_string(string line){
+//     string data;
+
+//     data = line.substr(line.find("=") + 1);  // delete miliseconds and "DATA="
+
+//     data.erase(remove(data.begin(), data.end(), '|'), data.end());  // delete "|"
+//     data.erase(remove(data.begin(), data.end(), ';'), data.end());  // delete ";"
+
+//     return data;
+// }
+
+
+
+
+
+// array<string, 3> extract_info(int info_index){
+//     std::ifstream infile("train_data/data_instrukcja.json");
+//     json data = json::parse(infile);  // parse json
+//     json firstAwiaParams = data["AwiaParams"][0];  // enter table AwiaParams
+//     json paramsArray = firstAwiaParams["params"][info_index];  // enter table Params
+//     array<string, 3> info;
+//     info[0] = paramsArray.value("key", "not found");  // get value "key"
+//     info[1] = to_string(paramsArray.at("awiabit"));  // get value "awiabit"
+//     info[2] = to_string(paramsArray.at("len"));  // get value "len"
+//     return info;
+// }
+
+
+
+
+
+// void byte_swap(string& text){  // Funtion swapping bytes from little endian to big endian, works only for numbers of length 8,16,24 and so on....
+//         int length = text.length();
+//         if(length<=2)return;  // no need to change bytes
+//         int x = length/2;
+//         char temp;
+//         for(int i=0;i<x;i+=2){
+//                 temp=text[i];
+//                 text[i]=text[length-2-i];
+//                 text[length-2-i]=temp;
+//                 temp=text[i+1];
+//                 text[i+1]=text[length-1-i];
+//                 text[length-1-i]=temp;
+//         }
+// }
+
+
+
+
+
+// void transform_info(string data, string info_name, int info_pos, int info_len,float &coord)
+// { 
+//     info_pos = info_pos/4;
+//     info_len = info_len/4;
+//     if(info_len<=2){
+//         string info = data.substr(info_pos, info_len);  // find wanted info in string 
+//         int info_int;
+//         istringstream(info) >> hex >> info_int;  // convert info to decimal
+//         // fprintf(stderr, "%s: %d \n", info_name.c_str(), info_int);  // print info
+//     }
+//     else if(info_pos==52){
+//         string info = data.substr(info_pos, info_len);  // find wanted info in string 
+//         byte_swap(info);
+//         float info_float;
+//         int info_int;
+//         istringstream(info) >> hex >> info_int;  // convert info to decimal
+//         coord = info_int;
+//     }
+//     else{
+//         string info = data.substr(info_pos,info_len);
+//         byte_swap(info);
+//         union ulf
+//         {
+//             unsigned long ul;
+//             float f;
+//         };
+//         ulf u;
+//         string str = info;
+//         stringstream ss(str);
+//         ss >> hex >> u.ul;
+//         float f = u.f;
+//         coord = u.f;
+//         // fprintf(stderr, "%s: %f \n", info_name.c_str(), f);
+//     }
+// }
+
+
+
+
+
+// void getCoords(string data, float& x, float& y, int& distance){
+//     //reading latitude from file
+//     float distance_temp;
+//     array<string, 3> current_info = extract_info(14);
+//     transform_info(data,current_info[0],stoi(current_info[1]),stoi(current_info[2]),x);
+
+//     //reading longitude from file
+//     current_info = extract_info(15);
+//     transform_info(data, current_info[0], stoi(current_info[1]), stoi(current_info[2]),y);
+
+//     current_info = extract_info(18);
+//     //reading distance from file
+//     transform_info(data, current_info[0], stoi(current_info[1]), stoi(current_info[2]),distance_temp);
+
+//     distance = distance_temp;
+// }
+
+
+
+
+
+// // This function converts decimal degrees to radians
+// double deg2rad(double deg) {
+//   return (deg * MATH_PI / 180);
+// }
+
+// //  This function converts radians to decimal degrees
+// double rad2deg(double rad) {
+//   return (rad * 180 / MATH_PI);
+// }
+
+
+
+
+
+// int32_t distanceCal(double lat1, double lon1, double lat2, double lon2, int64_t *odist)
+// {
+//     if ((lat1 == lat2) && (lon1 == lon2))
+//     {
+//         *odist = 0;
+//         return (int32_t)Result::SUCCESS;
+//     }
+//     double lat1r, lon1r, lat2r, lon2r, u, v;
+//     lat1r = deg2rad(lat1);
+//     lon1r = deg2rad(lon1);
+//     lat2r = deg2rad(lat2);
+//     lon2r = deg2rad(lon2);
+//     u = sin((lat2r - lat1r)/2);
+//     v = sin((lon2r - lon1r)/2);
+//     *odist = 1000 * 1000 * 2.0 * earthRadiusKm * asin(sqrt(u * u + cos(lat1r) * cos(lat2r) * v * v));
+
+//     return (int32_t)Result::SUCCESS;
+// }
+
+
+
+
+
+// //Reading routes from file
+// void readStations(vector<string>& route){
+//     fstream file_route("train_data/route.json");
+//     json route_json = json::parse(file_route);
+//     route = route_json.get<vector<string>>();
+// }
+
+// //Reading distaances from file
+// void readDistances(vector<int>& distance){
+//     fstream file_distance("train_data/distances.json");
+//     json distance_json = json::parse(file_distance);
+//     distance = distance_json.get<vector<int>>();
+// }
+
+// int findFirstStation(float& x,float& y){
+//     return 0;
+// }
+
+// void distanceOnStart(int &distance_on_start){
+//     float distance_on_start_temp;
+
+//     ifstream readFromFile("train_data/2022_07_22_08_10_Gliwice_Czestochowa_data");
+//     string line;
+//     getline(readFromFile,line);
+//     string data = prepare_string(line);
+
+//     array<string, 3> current_info = extract_info(18);
+//     transform_info(data, current_info[0], stoi(current_info[1]), stoi(current_info[2]), distance_on_start_temp);
+//     distance_on_start = distance_on_start_temp;
+// }
+
+
+
+
+
+
+
+
+
+
+// void* Core1::run()
+// { 
+//     ifstream readFromFile("train_data/2022_07_22_08_10_Gliwice_Czestochowa_data");
+//     string line;
+
+//     float x,y;
+//     int distance_from_start;
+//     int distance_on_start;
+//     float current_distance;
+
+//     int firststation = -1;
+//     int state = 0;
+
+//     vector<string> route;
+//     vector<int> distance;
+
+//     distanceOnStart(distance_on_start);
+
+//     while (!readFromFile.eof())
+//     {
+//         getline(readFromFile,line);
+//         string data = prepare_string(line);
+
+//         current_distance = (distance_from_start - distance_on_start);
+
+//         //reading coords, and distance from file
+//         getCoords(data, x, y, distance_from_start);
+//         cout << setprecision(8) << endl << "X:" << x << endl << "Y:" << y << endl;
+//         cout << "DistanceFromStart: " << current_distance / 1000 << endl;
+//         cout << "\n";
+
+//         if(firststation == -1){
+//             findFirstStation(x,y);
+//         }
+
+//         // readStations(route);
+//         // readDistances(distance);
+
+//         // sleep(0.9);
+//         usleep(100000);
+//     }
+
+//     return NULL;
+// }
 
 
 
